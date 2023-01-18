d2e553704676c33290fdb9792920a14bd74c6da1
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-01-18 22:32:00 +0530

    iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,6 +1,31 @@
+const { default: axios } = require("axios");
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseUrl
+    });
+  }
+
+  getAllCharacters = () => {
+    return this.api.get('/characters');
+  };
+ 
+  getOneCharacter = (characterId) => {
+    return this.api.get(`/characters/${characterId}`);
+  }
+ 
+  createCharacter = (characterInfo) => {
+    return this.api.post(`/characters`, characterInfo);
+  }
+ 
+  deleteCharacter = (characterId) => {
+    return this.api.delete(`/characters/${characterId}`);
+  }
+
+  editCharacter = (characterId, characterInfo) => {
+    return this.api.put(`/characters/${characterId}`, characterInfo);
   }
 
   getFullList () {
