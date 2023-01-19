29763393d442e404fb610b78d78f36697770a4ab
Nicole <92593875+ozzleme@users.noreply.github.com>
2023-01-19 22:52:07 +0530

    an attempt at getting the data using innerHTML

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,53 +1,86 @@
 
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.api = axios.create({
-			baseURL: baseUrl
-		})
-	}
+      baseURL: baseUrl
+    })
+  }
 
   // changed from getCharacter to getFullList (it was alreday there,see below)
   getFullList() {
-		return this.api
-				.get('/characters')
-				.then(result => console.log(result.data))
-				.catch(e => console.log(`Something went wrong getting all characters ${e}`));
-	}
- 
+    return this.api
+      .get('/characters')
+      .then(result => console.log(result.data))
+      .then(() => {
+      /*
+        Idea 1 
+        characters.forEach((element) => {
+          console.log(element.name); 
+      });
+  /*
+      // Idea 2 
+        /*for (let i = 0; i < 3; i++) {
+          const object = [i]
+          for (const key in object) {
+
+            console.log(`${key}: ${object[key]}`);
+        } */
+        // Idea 3 
+          //let txt = "";
+         // for (let x in characterObject) {
+           // txt += characterObject[x] + " ";
+           // document.getElementById("character-info").innerHTML = txt
+          //};
+          //}; 
+
+          // REALIZATION: it's an object of an array of objects - how do we access this data?
+      })
+      .catch(e => console.log(`Something went wrong getting all characters ${e}`));
+  }
+
   // changed to getOneRegister
   getOneRegister(characterId) {
     return this.api
       .get(`/characters/${characterId}`)
       .then(result => console.log(result.data))
-			.catch(e => console.log(`Something went wrong getting one character ${e}`));
+      .catch(e => console.log(`Something went wrong getting one character ${e}`));
   }
 
- // changed
+  // changed
   createOneRegister(characterInfo) {
     return this.api
       .post(`/characters`, characterInfo)
       .then(result => console.log(result.data))
-			.catch(e => console.log(`Something went wrong creating one character ${e}`));
+
+      // Idea 1 
+     /* function Person(first, last, age, eye) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
+        this.eyeColor = eye;
+      }
+      */
+      .catch(e => console.log(`Something went wrong creating one character ${e}`));
   }
- 
- // changed
+
+  // changed
   deleteOneRegister(characterId) {
     return this.api
       .delete(`/characters/${characterId}`)
       .then(result => console.log(result.data))
-			.catch(e => console.log(`Something went wrong deleting a character ${e}`));
+      .catch(e => console.log(`Something went wrong deleting a character ${e}`));
   }
 
- // changed
+  // changed
   updateOneRegister(characterId, characterInfo) {
     return this.api
-    .put(`/characters/${characterId}`, characterInfo)
-    .then(result => console.log(result.data))
-		.catch(e => console.log(`Something went wrong updating one character ${e}`));
+      .put(`/characters/${characterId}`, characterInfo)
+      .then(result => console.log(result.data))
+      .catch(e => console.log(`Something went wrong updating one character ${e}`));
   }
 
 }
-   
+
 
 /* getFullList () {
 
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,7 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
 	document.getElementById('fetch-all').addEventListener('click', function (event) {
-		charactersAPI.getFullList();
+		charactersAPI.getFullList()
 	});  
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
@@ -27,4 +27,6 @@ window.addEventListener('load', () => {
     charactersAPI.updateOneRegister(userInput);
 
   });
+
+
 });
